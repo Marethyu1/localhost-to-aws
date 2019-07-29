8c44720904db53f753de2506a5c4d85e47889a91
Stefan Hall <stefan.hall@xero.com>
2019-07-29 19:27:34 +1200

    Added resizing and nginx

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const sharp = require('sharp');
 const app = express();
 const port = process.env.PORT || 8080;
 const upload = multer({dest: 'uploads/'});
@@ -10,6 +11,7 @@ const images_folder = path.join(__dirname, `/uploads`);
 
 
 app.get('/', (req, res) => {
+  console.log("I have received a request");
   res.send("Hello world")
 });
 
@@ -28,9 +30,30 @@ app.get('/images', (req, res) => {
 app.get('/images/:id', (req, res) => {
   const id = req.params.id;
   const file_path = path.join(images_folder, `${id}`);
-  res.sendFile(file_path)
+  fs.createReadStream(file_path)
+    .pipe(res)
 });
 
+app.get('/images/:id/resize', (req, res) => {
+  const id = req.params.id
+  const roundedCorners = Buffer.from(
+    '<svg><rect x="0" y="0" width="200" height="200" rx="50" ry="50"/></svg>'
+  );
+
+  const roundedCornerResizer = sharp()
+      .resize(200, 200)
+      .composite([{
+        input: roundedCorners,
+        blend: 'dest-in'
+      }])
+      .png();
+
+  fs.createReadStream(path.join(images_folder, id))
+    .pipe(roundedCornerResizer)
+    .pipe(res)
+});
+
+
 app.listen(port, function() {
-  console.log(`App running on port ${8080}`)
+  console.log(`App running on port ${port}`)
 });
